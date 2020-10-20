3158f2c0e0d06b1be71f3961d16a9002e96e7248
Alejandro Castilla <contacto@alejandro-castilla.com>
2020-10-21 00:37:52 +0200

    #1 Add NotificationListener and add a button to grant notification access

---
+++ app/src/main/java/com/zedd/musicnetwork/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.zedd.musicnetwork;
+
+public interface ApplicationConstants {
+    String APPLICATION_TAG = "ZEDD_M_N";
+
+    // Intents actions
+    String NOTIFICATION_LISTENER_DATA = "com.zedd.musicnetwork.notification_listener_data";
+    String NOTIFICATION_LISTENER_DATA_TEXT_EXTRA = "com.zedd.musicnetwork.notification_listener_data.text";
+}
